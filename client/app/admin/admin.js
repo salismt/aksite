04a943e28d725757f8a58fb3ed25f706ebfb1af2
Koroluk, Andrew <Andrew.Koroluk@inin.com>
2014-09-22 14:06:25 -0400

    new photoManager Route

--- client/app/admin/admin.js
+++ client/app/admin/admin.js
@@ -1,11 +1,16 @@
 'use strict';
 
 angular.module('aksiteApp')
-  .config(function ($stateProvider) {
-    $stateProvider
-      .state('admin', {
-        url: '/admin',
-        templateUrl: 'app/admin/admin.html',
-        controller: 'AdminCtrl'
-      });
-  });
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('admin', {
+                url: '/admin',
+                templateUrl: 'app/admin/admin.html',
+                controller: 'AdminCtrl'
+            })
+            .state('photosManager', {
+                url: '/admin/photos',
+                templateUrl: 'app/admin/photosManager/photosManager.html',
+                controller: 'PhotosManagerCtrl'
+            });
+    });
