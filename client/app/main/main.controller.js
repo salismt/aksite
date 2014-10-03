f0c5a53fc3a8902e7db2135aee9ad3477ed459ee
Awk34 <koroluka@gmail.com>
2014-10-03 02:05:06 -0400

    changed hexbin location

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -274,49 +274,4 @@ angular.module('aksiteApp')
 				return idle;
 			});
 		}
-
-        /*
-		var margin = {top: 20, right: 20, bottom: 30, left: 40},
-            width = 900 - margin.left - margin.right,
-            height = 500 - margin.top - margin.bottom;
-
-        var color = d3.scale.linear()
-            .domain([0, 20])
-            .range(["orange", "orange"])
-            .interpolate(d3.interpolateLab);
-
-        var hexbin = d3.hexbin()
-            .size([width, height])
-            .radius(100);
-
-        var points = hexbin.centers();
-
-        var x = d3.scale.identity()
-            .domain([0, width]);
-
-        var y = d3.scale.linear()
-            .domain([0, height])
-            .range([height, 0]);
-
-        var svg = d3.select("#featured-photos").append("svg")
-			.attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom);
-
-        svg.append("clipPath")
-            .attr("id", "clip")
-            .append("rect")
-            .attr("class", "mesh")
-            .attr("width", width)
-            .attr("height", height);
-
-        svg.append("g")
-            .attr("clip-path", "url(#clip)")
-            .selectAll(".hexagon")
-            .data(hexbin(points))
-            .enter().append("path")
-            .attr("class", "hexagon")
-            .attr("d", hexbin.hexagon())
-            .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
-            .style("fill", 'url(#image)');
-            */
     });
