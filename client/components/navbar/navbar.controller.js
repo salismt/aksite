5cc0a219a20be169abbe70c86841911aec91efa5
Awk34 <koroluka@gmail.com>
2014-10-23 21:56:56 -0400

    code tweaks

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,42 +1,42 @@
 'use strict';
 
 angular.module('aksiteApp')
-  .controller('NavbarCtrl', function ($scope, $location, Auth) {
-    $scope.menu = [
-      {
-        'title': 'Home',
-        'link': '/'
-      },
-      {
-        'title': 'Résumé',
-        //'link': '/resume'
-        'link': 'https://www.linkedin.com/profile/view?id=168041753'
-      },
-      {
-        'title': 'Projects',
-        'link': '/projects'
-      },
-      {
-        'title': 'Photography',
-        'link': '/photos'
-      },
-      {
-        'title': 'Blog',
-        'link': '/blog'
-      }
-    ];
+    .controller('NavbarCtrl', function ($scope, $location, Auth) {
+        $scope.menu = [
+            {
+                'title': 'Home',
+                'link': '/'
+            },
+            {
+                'title': 'Résumé',
+                //'link': '/resume'
+                'link': 'https://www.linkedin.com/profile/view?id=168041753'
+            },
+            {
+                'title': 'Projects',
+                'link': '/projects'
+            },
+            {
+                'title': 'Photography',
+                'link': '/photos'
+            },
+            {
+                'title': 'Blog',
+                'link': '/blog'
+            }
+        ];
 
-    $scope.isCollapsed = true;
-    $scope.isLoggedIn = Auth.isLoggedIn;
-    $scope.isAdmin = Auth.isAdmin;
-    $scope.getCurrentUser = Auth.getCurrentUser;
+        $scope.isCollapsed = true;
+        $scope.isLoggedIn = Auth.isLoggedIn;
+        $scope.isAdmin = Auth.isAdmin;
+        $scope.getCurrentUser = Auth.getCurrentUser;
 
-    $scope.logout = function () {
-      Auth.logout();
-      $location.path('/login');
-    };
+        $scope.logout = function () {
+            Auth.logout();
+            $location.path('/login');
+        };
 
-    $scope.isActive = function (route) {
-      return route === $location.path();
-    };
-  });
+        $scope.isActive = function (route) {
+            return route === $location.path();
+        };
+    });
--- server/api/photo/photo.controller.js
+++ server/api/photo/photo.controller.js
@@ -18,7 +18,7 @@ exports.index = function (req, res) {
         if (err) {
             return handleError(res, err);
         }
-        return res.json(200, photos);
+        return res.status(200).json(photos);
     });
 };
 
