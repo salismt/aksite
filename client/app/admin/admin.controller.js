15049a766124ddbf90ca2e73ac577c834a7cbb58
Awk34 <koroluka@gmail.com>
2014-11-06 21:32:13 -0500

    Create admin page buttons, items controlled via ng-repeat-ed list

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -2,11 +2,38 @@
 
 angular.module('aksiteApp')
     .controller('AdminCtrl', function ($scope, $http, Auth, User) {
+        $scope.pages = [
+            {
+                name: "Users",
+                icon: "fa-user",
+                link: "users"
+            }, {
+                name: "Photos",
+                icon: "fa-photo",
+                link: "users"
+            }, {
+                name: "Projects",
+                icon: "fa-briefcase",
+                link: "users"
+            }, {
+                name: "Blog",
+                icon: "fa-file-text-o",
+                link: "users"
+            }, {
+                name: "Files",
+                icon: "fa-files-o",
+                link: "users"
+            }, {
+                name: "Settings",
+                icon: "fa-cog",
+                link: "users"
+            }
+        ];
 
         // Use the User $resource to fetch all users
         $scope.users = User.query();
 
-        $scope.delete = function (user) {
+        $scope.deleteUser = function (user) {
             User.remove({ id: user._id });
             angular.forEach($scope.users, function (u, i) {
                 if (u === user) {
