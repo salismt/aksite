ab9c4af2717fb23c6cecbdb6503eb3019fd319bd
Andrew Koroluk <koroluka@gmail.com>
2014-11-10 19:25:36 -0500

    fixed grid gallery

--- client/app/photos/gallery/gallery.controller.js
+++ client/app/photos/gallery/gallery.controller.js
@@ -1,24 +1,27 @@
 'use strict';
 
 angular.module('aksiteApp')
-    .controller('GalleryCtrl', function($scope, $stateParams, $http, Photo, Gallery) {
+    .controller('GalleryCtrl', function($scope, $stateParams, $http, $q, Photo, Gallery) {
         $scope.galleryId = $stateParams.galleryId;
 
-        $scope.gridGallery = new CBPGridGallery(document.getElementById('grid-gallery'));
-
-        //TODO: make galleries load asynchronously
+        //TODO: make photos load asynchronously
         $scope.photos = [];
         $http.get('/api/gallery/'+$stateParams.galleryId)
             .success(function(gallery) {
+                $scope.gridGallery = new CBPGridGallery(document.getElementById('grid-gallery'));
                 $scope.gallery = gallery;
+                var promises = [];
                 _.each(gallery.photos, function(photo) {
-                    $http.get('api/photos/'+photo)
+                    var promise = $http.get('api/photos/'+photo)
                         .success(function(photo) {
                             $scope.photos.push(photo);
-                            //console.log($scope.gridGallery);
-                            $scope.gridGallery._init();
                         });
+                    promises.push(promise);
                 });
+                $q.all(promises)
+                    .then(function(results) {
+                        $scope.gridGallery._init();
+                    });
             })
             .error(function(data, status) {
                 $scope.error = status;
