ed131e86cbb8e8503128165c52ca17e65c3dd8a2
Koroluk, Andrew <Andrew.Koroluk@inin.com>
2014-11-11 15:39:43 -0500

    Projects link to their individual pages, added `marked` content to details page

--- client/app/projects/project/project.controller.js
+++ client/app/projects/project/project.controller.js
@@ -7,6 +7,7 @@ angular.module('aksiteApp')
         $http.get('/api/projects/'+$stateParams.projectId)
             .success(function(project) {
                 $scope.project = project;
+                $scope.content = marked(project.info);
             })
             .error(function(data, status) {
                 $scope.error = status;
