24deabe761ea088a60faf4b43231d8ec228ea34c
Andrew Koroluk <koroluka@gmail.com>
2014-11-12 17:57:36 -0500

    jshint

--- client/app/admin/featuredManager/featuredManager.controller.js
+++ client/app/admin/featuredManager/featuredManager.controller.js
@@ -4,7 +4,7 @@ angular.module('aksiteApp')
     .controller('FeaturedmanagerCtrl', function($scope, $http) {
         $scope.newFeaturedSection = function() {
             $http.get('/api/featured/new')
-                .success(function(data, status, headers, config) {
+                .success(function(data, status) {
                     console.log(status);
                 });
         };
