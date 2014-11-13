ee7583c9a45f48ca4ebce9e0235f67432f21a1de
Andrew Koroluk <koroluka@gmail.com>
2014-11-12 20:20:20 -0500

    removed users code from base admin page

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('aksiteApp')
-    .controller('AdminCtrl', function($scope, $http, Auth, User) {
+    .controller('AdminCtrl', function($scope) {
         $scope.pages = [
             {
                 name: 'Users',
@@ -33,16 +33,4 @@ angular.module('aksiteApp')
                 link: 'admin/settings'
             }
         ];
-
-        // Use the User $resource to fetch all users
-        $scope.users = User.query();
-
-        $scope.deleteUser = function(user) {
-            User.remove({id: user._id});
-            angular.forEach($scope.users, function(u, i) {
-                if(u === user) {
-                    $scope.users.splice(i, 1);
-                }
-            });
-        };
     });
