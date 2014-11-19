56b15cbcea82d1457c2f7888e5e85996972a7c37
Koroluk, Andrew <Andrew.Koroluk@inin.com>
2014-11-19 14:19:36 -0500

    feat(photoManager): Make new photoManager for managing galleries
    Photo manager page shows all your galleries. Each has a delete button, which will mark the gallery for deletion. On hit 'Save Changes', the galleries will be deleted.

--- client/app/admin/photoManager/photoManager.controller.js
+++ client/app/admin/photoManager/photoManager.controller.js
@@ -2,12 +2,26 @@
 
 angular.module('aksiteApp')
     .controller('PhotomanagerCtrl', function($scope, $http) {
-        $scope.errors = {};
+        $scope.errors = [];
         $scope.loadingGalleries = true;
+        $scope.galleries = [];
+        $scope.galleryDeletions = [];
+        $scope.galleryChanges = [];
+        $scope.dirty = false;
 
         $http.get('/api/gallery')
-            .success(function(res, status) {
+            .success(function(res) {
                 $scope.galleries = res;
+                _.forEach($scope.galleries, function(gallery) {
+                    $http.get('/api/photos/'+gallery.featuredId)
+                        .success(function(res) {
+                            gallery.featured = res;
+                        })
+                        .error(function(res, status) {
+                            console.log(res);
+                            console.log(status);
+                        });
+                });
             })
             .error(function(res, status) {
                 console.log(res);
@@ -16,4 +30,37 @@ angular.module('aksiteApp')
             .finally(function() {
                 $scope.loadingGalleries = false;
             });
+
+        $scope.toggleGalleryDeletion = function(gallery) {
+            if(!gallery.deleted) {
+                gallery.deleted = true;
+                $scope.dirty = true;
+                $scope.galleryDeletions.push(gallery);
+            } else {
+                gallery.deleted = false;
+                _.remove($scope.galleryDeletions, function(thisGallery) {
+                    return thisGallery._id === gallery._id;
+                });
+                if($scope.galleryDeletions.length === 0) {
+                    $scope.dirty = false;
+                }
+            }
+        };
+
+        $scope.saveChanges = function() {
+            // Delete galleries
+            _.forEach($scope.galleryDeletions, function(gallery) {
+                $http.delete('/api/gallery/'+gallery._id)
+                    .success(function(res, status) {
+                        _.remove($scope.galleries, gallery);
+                        $scope.dirty = false;
+                        console.log(res);
+                        console.log(status);
+                    })
+                    .error(function(res, status) {
+                        console.log(res);
+                        console.log(status);
+                    });
+            });
+        };
     });
