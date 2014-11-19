21d22090b82e41930aa698159d058e85430614ba
Andrew Koroluk <koroluka@gmail.com>
2014-11-18 20:13:22 -0500

    refactor(gallery-editor): Change gallery editor route
    Change /admin/photos/galleryEditor to /admin/photos/gallery-editor

--- client/app/admin/galleryEditor/galleryEditor.js
+++ client/app/admin/galleryEditor/galleryEditor.js
@@ -4,7 +4,7 @@ angular.module('aksiteApp')
     .config(function($stateProvider) {
         $stateProvider
             .state('galleryEditor', {
-                url: '/admin/photos/galleryEditor',
+                url: '/admin/photos/gallery-editor',
                 templateUrl: 'app/admin/galleryEditor/galleryEditor.html',
                 controller: 'GalleryEditorCtrl'
             });
