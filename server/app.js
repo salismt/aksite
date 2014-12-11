be496589cbbae84b6854f65942b2fbc5a0690393
Andrew Koroluk <koroluka@gmail.com>
2014-12-11 01:11:00 -0500

    feat(new-relic): Add New Relic

--- server/app.js
+++ server/app.js
@@ -7,9 +7,14 @@
 // Set default node environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-var express = require('express');
-var mongoose = require('mongoose');
 var config = require('./config/environment');
+
+if(config.env === 'production') {
+    require('newrelic');
+}
+
+var express = require('express'),
+    mongoose = require('mongoose');
 
 // Connect to database
 mongoose.connect(config.mongo.uri, config.mongo.options);
@@ -20,9 +25,9 @@ if(config.seedDB) {
 }
 
 // Setup server
-var app = express();
-var server = require('http').createServer(app);
-var socketio = require('socket.io').listen(server);
+var app = express(),
+    server = require('http').createServer(app),
+    socketio = require('socket.io').listen(server);
 require('./config/socketio')(socketio);
 require('./config/express')(app);
 require('./routes')(app);
