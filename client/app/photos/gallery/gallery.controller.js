a78a5818c06e6a2e80768ee9f9af3d421776c80d
Andrew Koroluk <koroluka@gmail.com>
2014-12-14 04:45:12 -0500

    feat(photos): Fix PhotoSwipe stuff, restyle photo grid

--- client/app/photos/gallery/gallery.controller.js
+++ client/app/photos/gallery/gallery.controller.js
@@ -5,61 +5,62 @@ angular.module('aksiteApp')
         $scope.galleryId = $stateParams.galleryId;
         $scope.errors = [];
         $scope.photos = [];
-        var galleryElement = document.querySelectorAll('.gallery-grid')[0];
+        $scope.pswpPhotos = [];
+        var galleryElement = document.querySelectorAll('.photo-gallery .grid-wrap .grid')[0];
+        var items = [];
 
-        //TODO: make photos load asynchronously
         $http.get('/api/gallery/'+$stateParams.galleryId)
             .success(function(gallery) {
                 $scope.gallery = gallery;
 
                 if($scope.gallery.photos.length < 1) return $scope.noPhotos = true;
 
-                $scope.pswpPhotos = [];
-
+                var photoPromises = [];
                 _.each(gallery.photos, function(photo) {
-                    $http.get('api/photos/'+photo)
+                    photoPromises.push($http.get('api/photos/'+photo)
                         .success(function(photo) {
-                            photo.done = true;
                             photo.index = $scope.photos.length;
                             $scope.photos.push(photo);
                             $scope.pswpPhotos.push(photoToPSWP(photo, $scope.photos.length-1));
-                        });
+                        }));
                 });
-                console.log($scope.photos);
-                console.log($scope.pswpPhotos);
+
+                $q.all(photoPromises)
+                    .then(function() {
+                        //TODO: If I take the timeout out, photo grid doesn't load properly
+                        setTimeout(function() {
+                            new Masonry(document.querySelector('.photo-gallery'), {
+                                itemSelector: 'li',
+                                columnWidth: document.querySelector('.photo-gallery .grid-sizer')
+                            });
+                        }, 30);
+                    });
             })
             .error(function(data, status) {
                 $scope.errors.push(status);
             });
 
-        $scope.onThumbnailsClick = function(event, index) {
-            //console.log(event.target);
+        $scope.onThumbnailsClick = function(index) {
             openPhotoSwipe(index);
         };
 
-        var items = [];
-
         var openPhotoSwipe = function(index, disableAnimation) {
-            //console.log(index);
             var pswpElement = document.querySelectorAll('.pswp')[0],
                 gallery,
-                options;
+                options,
+                vscroll = document.body.scrollTop;
 
             if(!items || items.length == 0) {
-                // build slides array, only once
                 items = parseThumbnailElements(galleryElement);
             }
 
-            // define options (if needed)
             options = {
                 index: index,
-
                 getThumbBoundsFn: function(index) {
                     // See Options->getThumbBoundsFn section of docs for more info
                     var thumbnail = items[index].el.children[0],
                         pageYScroll = window.pageYOffset || document.documentElement.scrollTop,
                         rect = thumbnail.getBoundingClientRect();
-
                     return {x:rect.left, y:rect.top + pageYScroll, w:rect.width};
                 }
             };
@@ -68,42 +69,38 @@ angular.module('aksiteApp')
                 options.hideAnimationDuration = options.showAnimationDuration = 0;
             }
 
-            // Pass data to PhotoSwipe and initialize it
             gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
             gallery.init();
+            gallery.listen('destroy', function() {
+                // Temporary workaround for PhotoSwipe scroll-to-top on close bug
+                setTimeout(function() {
+                    window.scrollTo(null, vscroll);
+                }, 5);
+            });
         };
 
         var parseThumbnailElements = function(el) {
             var thumbElements = el.childNodes,
-                numNodes = thumbElements.length,
                 items = [],
-                el,
                 childElements,
-                thumbnailEl,
                 size,
                 item;
 
-            for(var i = 0; i < numNodes; i++) {
-                el = thumbElements[i];
-
-                // include only element nodes
-                if(el.nodeType !== 1) {
-                    continue;
+            _.forEach(thumbElements, function(el) {
+                if(el.nodeType !== 1 || el.localName !== 'li' || el.className === 'grid-sizer') {
+                    return;
                 }
 
-                childElements = el.children;
-                //console.log(childElements);
-
+                childElements = el.children[0].children;
                 size = el.getAttribute('data-size').split('x');
 
                 // create slide object
                 item = {
                     src: el.getAttribute('id'),
                     w: parseInt(size[0], 10),
-                    h: parseInt(size[1], 10)
+                    h: parseInt(size[1], 10),
+                    el: el// save link to element for getThumbBoundsFn
                 };
-
-                item.el = el; // save link to element for getThumbBoundsFn
 
                 if(childElements.length > 0) {
                     item.msrc = childElements[0].getAttribute('src'); // thumbnail url
@@ -113,16 +110,15 @@ angular.module('aksiteApp')
                 }
 
                 items.push(item);
-            }
+            });
 
-            console.log(items);
             return items;
         };
 
         function photoToPSWP(photo, index) {
             return {
-                src: 'api/upload/'+photo.fileId,
-                msrc: 'api/upload/'+photo.thumbnailId,
+                src: 'api/upload/' + photo.fileId,
+                msrc: 'api/upload/' + photo.thumbnailId,
                 w: photo.width,
                 h: photo.height,
                 index: index
