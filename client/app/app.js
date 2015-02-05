a54c686c7c8ae7f405f5235907b5d4e51fb793c1
Andrew Koroluk <koroluka@gmail.com>
2015-02-05 00:53:42 -0500

    fix(app): Add ngMessages angular dependency

--- client/app/app.js
+++ client/app/app.js
@@ -8,7 +8,8 @@ angular.module('aksiteApp', [
     'ui.router',
     'ui.bootstrap',
     'angularFileUpload',
-    'ngMaterial'
+    'ngMaterial',
+    'ngMessages'
 ])
     .config(function($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
         $urlRouterProvider
