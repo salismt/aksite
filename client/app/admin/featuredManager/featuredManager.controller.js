633787db4e6d9106dcd8590a8533d26f0eedddd8
Andrew Koroluk <koroluka@gmail.com>
2015-02-06 03:03:02 -0500

    feat(featured): Add initial list of featured items

--- client/app/admin/featuredManager/featuredManager.controller.js
+++ client/app/admin/featuredManager/featuredManager.controller.js
@@ -2,10 +2,25 @@
 
 angular.module('aksiteApp')
     .controller('FeaturedmanagerCtrl', function($scope, $http) {
+        $scope.loadingItems = true;
+        $http.get('/api/featured/items')
+            .success(function(data) {
+                console.log(data);
+                $scope.items = data;
+            })
+            .error(function(err, status) {
+                console.log(err);
+                console.log(status);
+            });
+
         $scope.newFeaturedSection = function() {
             $http.get('/api/featured/new')
                 .success(function(data, status) {
                     console.log(status);
                 });
+        };
+
+        $scope.deleteItem = function(item) {
+            //TODO
         };
     });
