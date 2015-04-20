afac10ffc5f316107f3d702ef99c0b20c9f19b2d
Andrew Koroluk <koroluka@gmail.com>
2015-04-19 23:28:36 -0400

    refactor(config): Remove unused var

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -19,8 +19,5 @@ module.exports = {
         process.env.MONGOHQ_URL ||
         process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME ||
         'mongodb://localhost/aksite'
-    },
-
-    // Directory to upload to
-    uploadDir: '/aksite/data/'
+    }
 };
