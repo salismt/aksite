885a2caeda150f9c6e02f2b107d74eaa35d6e4ca
Andrew Koroluk <koroluka@gmail.com>
2015-04-20 11:45:37 -0400

    feat(routes): Add a 410 error view and send {root}/wordpress* there

--- server/components/errors/index.js
+++ server/components/errors/index.js
@@ -20,3 +20,20 @@ module.exports[404] = function pageNotFound(req, res) {
         res.render(viewFilePath);
     });
 };
+
+module.exports[410] = function pageGone(req, res) {
+    var viewFilePath = '410';
+    var statusCode = 410;
+    var result = {
+        status: statusCode
+    };
+
+    res.status(result.status);
+    res.render(viewFilePath, function(err) {
+        if(err) {
+            return res.json(result, result.status);
+        }
+
+        res.render(viewFilePath);
+    });
+};
