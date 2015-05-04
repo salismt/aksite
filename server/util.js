01d6b5269700101d657936303540c750ae12fab8
Andrew Koroluk <koroluka@gmail.com>
2015-05-04 04:12:39 -0400

    feat(util): Create server utility file
    Create first function, createThumbnail, which makes a thumbnail in GridFS

---
+++ server/util.js
@@ -0,0 +1,70 @@
+'use strict';
+
+/**
+ * Server Utility Functions
+ */
+
+var _ = require('lodash'),
+    q = require('q'),
+    //request = require('request'),
+    //path = require('path'),
+    config = require('./config/environment'),
+    mongoose = require('mongoose'),
+    gm = require('gm'),
+    Grid = require('gridfs-stream'),
+    gfs,
+    conn = mongoose.createConnection(config.mongo.uri);
+
+Grid.mongo = mongoose.mongo;
+
+conn.once('open', function(err) {
+    if(err) console.log(err);
+    else gfs = Grid(conn.db);
+});
+
+/**
+ * Creates a thumbnail in GridFS based on the ID passed
+ *
+ * @param {ObjectId} id         - The ID of the image in GridFS to create a thumbnail of
+ * @param {Object} [options]    - Optional options
+ * @param {number} [options.height=200] - The height of the thumbnail, in pixels
+ * @param {number} [options.width=200] - The width of the thumbnail, in pixels
+ * @param {number} [options.quality=90] - The quality of the thumbnail
+ */
+exports.createThumbnail = function(id, options) {
+    var deferred = q.defer(),
+        thumbnail = {};
+
+    options = options ? options : {};
+    _.defaults(options, {
+        height: 200,
+        width: 200,
+        quality: 90
+    });
+
+    var stream = gfs.createReadStream({_id: id});
+    stream.on('error', deferred.reject);
+    gm(stream, id)
+        .size({bufferStream: true}, function(err, size) {
+            thumbnail.width = size.width;
+            thumbnail.height = size.height;
+            this.resize(options.height, options.width, "^");
+            this.crop(options.height, options.width, 0, 0);
+            this.quality(options.quality);
+            this.stream(function(err, outStream) {
+                if(err) return deferred.reject(err);
+                else {
+                    var writestream = gfs.createWriteStream({});
+                    writestream.on('close', function(thumbnailFile) {
+                        thumbnail.file = thumbnailFile;
+                        thumbnail.id = thumbnailFile._id;
+
+                        return deferred.resolve(thumbnail);
+                    });
+                    outStream.pipe(writestream);
+                }
+            });
+        });
+
+    return deferred.promise;
+};
