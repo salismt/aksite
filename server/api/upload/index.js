dc8b82dbfc16b492683fb8e3d556a4c2e03acd95
Andrew Koroluk <koroluka@gmail.com>
2015-05-06 23:29:39 -0400

    feat(uploads): Remove showSize

--- server/api/upload/index.js
+++ server/api/upload/index.js
@@ -10,7 +10,6 @@ router.get('/', auth.hasRole('admin'), controller.index);
 router.get('/clean', auth.hasRole('admin'), controller.clean);
 router.get('/count', auth.hasRole('admin'), controller.count);
 router.get('/:id', controller.show);
-router.get('/:id/size', auth.hasRole('admin'), controller.showSize);
 router.post('/', auth.hasRole('admin'), controller.create);
 //router.put('/:id', controller.update);
 //router.patch('/:id', controller.update);
--- server/api/upload/upload.controller.js
+++ server/api/upload/upload.controller.js
@@ -69,25 +69,6 @@ exports.count = function(req, res) {
     });
 };
 
-//
-exports.showSize = function(req, res) {
-    if(!isValidObjectId(req.params.id)) {
-        return res.status(400).send('Invalid ID');
-    }
-    gfs.exist({_id: req.params.id}, function(err, found) {
-        if(err) return util.handleError(err);
-        else if(!found) return res.status(404).end();
-        else {
-            var readStream = gfs.createReadStream({_id: req.params.id});
-            readStream.on('error', handleGridStreamErr(res));
-            gm(readStream, req.params.id).size(function(err, size) {
-                if(err) return util.handleError(res, err);
-                else return res.status(200).json(size);
-            });
-        }
-    });
-};
-
 // Creates a new file in the DB.
 exports.create = function(req, res) {
     var form = gridform({db: conn.db, mongo: mongoose.mongo});
