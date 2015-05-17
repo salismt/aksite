8d615841336afbfdf6d36b1d87ae9a9ef6f6cb44
Andrew Koroluk <koroluka@gmail.com>
2015-05-17 13:47:43 -0400

    fix(posts): Use proper API path in posts spec

--- server/api/post/post.spec.js
+++ server/api/post/post.spec.js
@@ -4,11 +4,11 @@ var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
 
-describe('GET /api/blog/posts', function() {
+describe('GET /api/posts', function() {
 
     it('should respond with JSON array', function(done) {
         request(app)
-            .get('/api/blog/posts')
+            .get('/api/posts')
             .expect(200)
             .expect('Content-Type', /json/)
             .end(function(err, res) {
