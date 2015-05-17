e167551de1a397c4d87646c4f162d51d403472e9
Andrew Koroluk <koroluka@gmail.com>
2015-05-17 13:46:48 -0400

    feat(resume): Make initial resume page with static data

--- client/app/resume/resume.controller.js
+++ client/app/resume/resume.controller.js
@@ -1,6 +1,13 @@
 'use strict';
 
 angular.module('aksiteApp')
-    .controller('ResumeCtrl', function($scope) {
-        $scope.message = 'Hello';
+    .controller('ResumeCtrl', function($scope, $http) {
+        $http.get('/assets/linkedin_profile.json')
+            .success(function(data) {
+                console.log(data);
+                $scope.profile = data;
+            })
+            .error(function(err) {
+                console.log(err);
+            });
     });
