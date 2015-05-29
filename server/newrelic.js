43c9fc1175efd870a914dbf27339b901ec45668b
Andrew Koroluk <koroluka@gmail.com>
2015-05-29 11:49:48 -0400

    feat(new relic): Ignore 410 and 401 error codes

--- server/newrelic.js
+++ server/newrelic.js
@@ -5,20 +5,23 @@
  * description of configuration variables and their potential values.
  */
 exports.config = {
-  /**
-   * Array of application names.
-   */
-  app_name : ['aksite'],
-  /**
-   * Your New Relic license key.
-   */
-  license_key : '731fb34b032418b1eaa2d61ed8b46904238a9526',
-  logging : {
     /**
-     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
-     * issues with the agent, 'info' and higher will impose the least overhead on
-     * production applications.
+     * Array of application names.
      */
-    level : 'info'
-  }
+    app_name: ['aksite'],
+    /**
+     * Your New Relic license key.
+     */
+    license_key: '731fb34b032418b1eaa2d61ed8b46904238a9526',
+    logging: {
+        /**
+         * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+         * issues with the agent, 'info' and higher will impose the least overhead on
+         * production applications.
+         */
+        level: 'info'
+    },
+    error_collector: {
+        ignore_status_codes: [404, 410, 401]
+    }
 };
