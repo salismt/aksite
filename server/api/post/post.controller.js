556254f153b1e9c127c4be587d1c8fa74e230cad
Andrew Koroluk <koroluka@gmail.com>
2015-05-31 05:22:15 -0400

    style(posts):

--- server/api/post/post.controller.js
+++ server/api/post/post.controller.js
@@ -173,124 +173,119 @@ exports.create = function(req, res) {
 
 // Updates an existing post in the DB.
 exports.update = function(req, res) {
-    if(config.userRoles.indexOf(req.user.role) < config.userRoles.indexOf('admin')) {
+    if(config.userRoles.indexOf(req.user.role) < config.userRoles.indexOf('admin'))
         return res.status(401).send('You need to be an admin to create posts');
-    }
-    if(!util.isvalidobjectId(req.params.id)) {
+    if(!util.isvalidobjectId(req.params.id))
         return res.status(400).send('Invalid ID');
-    }
+
     var form = gridform({db: conn.db, mongo: mongoose.mongo});
 
     Post.findById(req.params.id, function(err, post) {
-        if(err) {
-            return util.handleError(res, err);
-        } else if(!post) {
-            return res.status(404).end();
-        } else {
-            form.parse(req, function(err, fields, files) {
-                if(err) return util.handleError(res, err);
+        if(err) return util.handleError(res, err);
+        else if(!post) return res.status(404).end();
 
-                if(fields._id) {
-                    delete fields._id;
-                }
+        form.parse(req, function(err, fields, files) {
+            if(err) return util.handleError(res, err);
 
-                /**
-                 * file.name            - the uploaded file name
-                 * file.type            - file type per [mime](https://github.com/bentomas/node-mime)
-                 * file.size            - uploaded file size (file length in GridFS) named "size" for compatibility
-                 * file.path            - same as file.name. included for compatibility
-                 * file.lastModified    - included for compatibility
-                 * file.root            - the root of the files collection used in MongoDB ('fs' here means the full collection in mongo is named 'fs.files')
-                 * file.id              - the ObjectId for this file
-                 * @type {file}
-                 */
-                var file = files.file;
+            if(fields._id)
+                delete fields._id;
 
-                if(fields.newImage && (_.isNull(file) || _.isUndefined(file)) )
-                    return res.status(400).send(new Error('No file'));
+            /**
+             * file.name            - the uploaded file name
+             * file.type            - file type per [mime](https://github.com/bentomas/node-mime)
+             * file.size            - uploaded file size (file length in GridFS) named "size" for compatibility
+             * file.path            - same as file.name. included for compatibility
+             * file.lastModified    - included for compatibility
+             * file.root            - the root of the files collection used in MongoDB ('fs' here means the full collection in mongo is named 'fs.files')
+             * file.id              - the ObjectId for this file
+             * @type {file}
+             */
+            var file = files.file;
 
-                console.log(file);
-                console.log(fields);
+            if(fields.newImage && (_.isNull(file) || _.isUndefined(file)))
+                return res.status(400).send(new Error('No file'));
 
-                //TODO
-                var sanitised = null;
+            console.log(file);
+            console.log(fields);
 
-                if(sanitised !== null) {
-                    return res.status(400).send(sanitised);
-                } else {
-                    var postModel = {};
-                    if(fields.title && typeof fields.title == 'string')
-                        postModel.title = fields.title;
-                    if(fields.subheader && typeof fields.subheader == 'string')
-                        postModel.subheader = fields.subheader;
-                    if(fields.alias && typeof fields.alias == 'string')
-                        postModel.alias = fields.alias;
-                    if(!_.isNull(fields.hidden) || !_.isUndefined(fields.hidden))
-                        postModel.hidden = fields.hidden ? true : false;
-                    if(fields.content && typeof fields.content == 'string')
-                        postModel.content = fields.content;
-                    if(fields.categories)
-                        postModel.categories = fields.categories;
-                    if(fields.date)
-                        postModel.date = new Date(fields.date);
+            //TODO
+            var sanitised = null;
 
-                    if(fields.newImage) {
-                        if(post.imageId) {
-                            gfs.remove({_id: post.imageId}, function (err) {
-                                if (err) return util.handleError(err);
-                                else console.log('deleted imageId');
-                            });
-                            gfs.remove({_id: post.thumbnailId}, function (err) {
-                                if (err) return util.handleError(err);
-                                else console.log('deleted thumbnailId');
-                            });
-                        }
+            if(sanitised !== null) {
+                return res.status(400).send(sanitised);
+            } else {
+                var postModel = {};
+                if(fields.title && typeof fields.title == 'string')
+                    postModel.title = fields.title;
+                if(fields.subheader && typeof fields.subheader == 'string')
+                    postModel.subheader = fields.subheader;
+                if(fields.alias && typeof fields.alias == 'string')
+                    postModel.alias = fields.alias;
+                if(!_.isNull(fields.hidden) || !_.isUndefined(fields.hidden))
+                    postModel.hidden = fields.hidden ? true : false;
+                if(fields.content && typeof fields.content == 'string')
+                    postModel.content = fields.content;
+                if(fields.categories)
+                    postModel.categories = fields.categories;
+                if(fields.date)
+                    postModel.date = new Date(fields.date);
 
-                        postModel.imageId = file.id;
-
-                        // Thumbnail generation
-                        var stream = gfs.createReadStream({_id: file.id});
-                        stream.on('error', util.handleError(res));
-                        gm(stream, file.id)
-                            .size({bufferStream: true}, function(err, size) {
-                                postModel.width = size.width;
-                                postModel.height = size.height;
-                                this.resize(null, 400);
-                                this.quality(90);
-                                this.stream(function(err, outStream) {
-                                    if(err) return res.status(500).end();
-                                    else {
-                                        var writestream = gfs.createWriteStream({filename: file.name});
-                                        writestream.on('close', function(thumbFile) {
-                                            console.log(file.name+' -> (thumb)'+thumbFile._id);
-                                            postModel.thumbnailId = thumbFile._id;
-
-                                            var updated = _.assign(post, postModel);
-                                            return updated.save(function(err) {
-                                                if(err) {
-                                                    return util.handleError(res, err);
-                                                } else {
-                                                    return res.status(200).json(post);
-                                                }
-                                            });
-                                        });
-                                        outStream.pipe(writestream);
-                                    }
-                                });
-                            });
-                    } else {
-                        var updated = _.assign(post, postModel);
-                        return updated.save(function(err) {
-                            if(err) {
-                                return util.handleError(res, err);
-                            } else {
-                                return res.status(200).json(post);
-                            }
+                if(fields.newImage) {
+                    if(post.imageId) {
+                        gfs.remove({_id: post.imageId}, function (err) {
+                            if (err) return util.handleError(err);
+                            else console.log('deleted imageId');
+                        });
+                        gfs.remove({_id: post.thumbnailId}, function (err) {
+                            if (err) return util.handleError(err);
+                            else console.log('deleted thumbnailId');
                         });
                     }
+
+                    postModel.imageId = file.id;
+
+                    // Thumbnail generation
+                    var stream = gfs.createReadStream({_id: file.id});
+                    stream.on('error', util.handleError(res));
+                    gm(stream, file.id)
+                        .size({bufferStream: true}, function(err, size) {
+                            postModel.width = size.width;
+                            postModel.height = size.height;
+                            this.resize(null, 400);
+                            this.quality(90);
+                            this.stream(function(err, outStream) {
+                                if(err) return res.status(500).end();
+                                else {
+                                    var writestream = gfs.createWriteStream({filename: file.name});
+                                    writestream.on('close', function(thumbFile) {
+                                        console.log(file.name+' -> (thumb)'+thumbFile._id);
+                                        postModel.thumbnailId = thumbFile._id;
+
+                                        var updated = _.assign(post, postModel);
+                                        return updated.save(function(err) {
+                                            if(err) {
+                                                return util.handleError(res, err);
+                                            } else {
+                                                return res.status(200).json(post);
+                                            }
+                                        });
+                                    });
+                                    outStream.pipe(writestream);
+                                }
+                            });
+                        });
+                } else {
+                    var updated = _.assign(post, postModel);
+                    return updated.save(function(err) {
+                        if(err) {
+                            return util.handleError(res, err);
+                        } else {
+                            return res.status(200).json(post);
+                        }
+                    });
                 }
-            });
-        }
+            }
+        });
     });
 };
 
