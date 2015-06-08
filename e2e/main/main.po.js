dfff46d6c0080abe54702a8c9a2552aab44599e0
Andrew Koroluk <koroluka@gmail.com>
2015-06-08 02:29:15 -0400

    feat(e2e): Make e2e test run

--- e2e/main/main.po.js
+++ e2e/main/main.po.js
@@ -6,9 +6,9 @@
 'use strict';
 
 var MainPage = function() {
-  this.heroEl = element(by.css('.hero-unit'));
-  this.h1El = this.heroEl.element(by.css('h1'));
-  this.imgEl = this.heroEl.element(by.css('img'));
+    this.poweredByEl = element(by.css('.vendor-section'));
+    this.h1El = this.poweredByEl.element(by.css('h1'));
+    this.imgContainerEl = this.poweredByEl.element(by.css('#vendor-container'));
 };
 
 module.exports = new MainPage();
