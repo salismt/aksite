c142367fae334a46d726d1cb452b32271f220e7b
Andrew Koroluk <koroluka@gmail.com>
2015-07-09 15:56:40 -0400

    style(client:post:controller): JSCS

--- client/app/blog/post/post.controller.js
+++ client/app/blog/post/post.controller.js
@@ -4,14 +4,14 @@ angular.module('aksiteApp')
     .controller('PostCtrl', function($rootScope, $scope, $stateParams, $http) {
         $scope.postId = $stateParams.postId;
 
-        $http.get('api/posts/'+$scope.postId)
+        $http.get('api/posts/' + $scope.postId)
             .success(function(post) {
                 $scope.post = post;
 
                 $rootScope.title += ' | ' + post.title;
 
                 $scope.post.content = marked($scope.post.content);
-                $scope.post.date = moment($scope.post.date).format("LL");
+                $scope.post.date = moment($scope.post.date).format('LL');
             })
             .error(function(err) {
                 console.log(err);
