7fbfd10fdd6f92b5bbf970e1796ff43e4dce5630
Andrew Koroluk <koroluka@gmail.com>
2015-07-13 16:03:30 -0400

    fix(grunt): add client js back to livereload

--- Gruntfile.js
+++ Gruntfile.js
@@ -98,6 +98,7 @@ module.exports = function(grunt) {
                 files: [
                     '{.tmp,<%= yeoman.client %>}/{app,components}/**/*.css',
                     '{.tmp,<%= yeoman.client %>}/{app,components}/**/*.html',
+                    '.tmp/{app,components}/**/*.js',
                     '!{.tmp,<%= yeoman.client %>}{app,components}/**/*.spec.js',
                     '!{.tmp,<%= yeoman.client %>}/{app,components}/**/*.mock.js',
                     '<%= yeoman.client %>/assets/images/{,*//*}*.{png,jpg,jpeg,gif,webp,svg}'
