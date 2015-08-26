60b31bdbcc7566367245fb082dee2fc2daf4ca2b
Andrew Koroluk <koroluka@gmail.com>
2015-08-26 19:10:16 -0400

    feat(server): remove wordpress 410

--- server/routes.js
+++ server/routes.js
@@ -27,14 +27,9 @@ module.exports = function(app) {
     app.route('/:url(api|auth|components|app|bower_components|assets)/*')
         .get(errors[404]);
 
-    // Hopefully this will tell Wordpress to piss off and stop pinging my site
-    app.route('/wordpress*')
-        .get(errors[410]);
-
     // All other routes should redirect to the index.html
     app.route('/*')
         .get(function(req, res) {
             res.sendfile(path.resolve(app.get('appPath') + '/index.html'));
         });
 };
-
