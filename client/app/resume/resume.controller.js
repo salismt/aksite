f74e25a470fd47393cd5d0af2ac109f1826b4e93
Andrew Koroluk <koroluka@gmail.com>
2015-08-27 14:36:11 -0400

    feat(timeline): add badges section

--- client/app/resume/resume.controller.js
+++ client/app/resume/resume.controller.js
@@ -13,7 +13,22 @@ angular.module('aksiteApp')
                     dateLine2: moment(new Date()).format('YYYY'),
                     image: 'assets/images/portrait_2014.jpg',
                     heading: 'Heading',
-                    content: 'Drumstick tenderloin hamburger swine tail. Cow porchetta hamburger, tongue ground round ball tip sausage corned beef pork chop beef spare ribs bresaola. Jowl tail pancetta pork landjaeger. Sausage chicken andouille meatball, ham hock doner short ribs. Picanha turducken porchetta pork belly boudin hamburger. Jowl beef ribs tenderloin, shankle short ribs ribeye hamburger porchetta andouille chicken ground round cow ball tip. Salami ground round cow, rump hamburger chuck pork loin landjaeger short loin doner alcatra ball tip.'
+                    content: 'Drumstick tenderloin hamburger swine tail. Cow porchetta hamburger, tongue ground round ball tip sausage corned beef pork chop beef spare ribs bresaola. Jowl tail pancetta pork landjaeger. Sausage chicken andouille meatball, ham hock doner short ribs. Picanha turducken porchetta pork belly boudin hamburger. Jowl beef ribs tenderloin, shankle short ribs ribeye hamburger porchetta andouille chicken ground round cow ball tip. Salami ground round cow, rump hamburger chuck pork loin landjaeger short loin doner alcatra ball tip.',
+                    badges: [{
+                        src: 'assets/images/portrait_2014.jpg'
+                    }, {
+                        src: 'assets/images/portrait_2014.jpg'
+                    }, {
+                        src: 'assets/images/portrait_2014.jpg'
+                    }, {
+                        src: 'assets/images/portrait_2014.jpg'
+                    }, {
+                        src: 'assets/images/portrait_2014.jpg'
+                    }, {
+                        src: 'assets/images/portrait_2014.jpg'
+                    }, {
+                        src: 'assets/images/portrait_2014.jpg'
+                    }]
                 }];
             })
             .error(function(err) {
