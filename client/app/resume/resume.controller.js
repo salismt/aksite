c847403e1dfd9c5a5dae8e7ac4f1f496008ecdbf
Andrew Koroluk <koroluka@gmail.com>
2015-08-27 00:38:51 -0400

    feat(timeline): remove seed data from timeline directive, include via isolated scope

--- client/app/resume/resume.controller.js
+++ client/app/resume/resume.controller.js
@@ -6,6 +6,15 @@ angular.module('aksiteApp')
             .success(function(data) {
                 console.log(data);
                 $scope.profile = data;
+
+                $scope.items = [{
+                    date: new Date(),
+                    dateLine1: moment(new Date()).format('MMM Do'),
+                    dateLine2: moment(new Date()).format('YYYY'),
+                    image: 'assets/images/portrait_2014.jpg',
+                    heading: 'Heading',
+                    content: 'Drumstick tenderloin hamburger swine tail. Cow porchetta hamburger, tongue ground round ball tip sausage corned beef pork chop beef spare ribs bresaola. Jowl tail pancetta pork landjaeger. Sausage chicken andouille meatball, ham hock doner short ribs. Picanha turducken porchetta pork belly boudin hamburger. Jowl beef ribs tenderloin, shankle short ribs ribeye hamburger porchetta andouille chicken ground round cow ball tip. Salami ground round cow, rump hamburger chuck pork loin landjaeger short loin doner alcatra ball tip.'
+                }];
             })
             .error(function(err) {
                 console.log(err);
--- client/components/timeline/timeline.directive.js
+++ client/components/timeline/timeline.directive.js
@@ -4,20 +4,10 @@ angular.module('aksiteApp')
     .directive('timeline', function() {
         return {
             scope: {
-
+                items: '='
             },
             templateUrl: 'components/timeline/timeline.html',
             restrict: 'EA',
-            controller: function($scope) {
-                $scope.items = [{
-                    date: new Date(),
-                    dateLine1: moment(new Date()).format('MMM Do'),
-                    dateLine2: moment(new Date()).format('YYYY'),
-                    image: 'assets/images/portrait_2014.jpg',
-                    heading: 'Heading',
-                    content: 'Drumstick tenderloin hamburger swine tail. Cow porchetta hamburger, tongue ground round ball tip sausage corned beef pork chop beef spare ribs bresaola. Jowl tail pancetta pork landjaeger. Sausage chicken andouille meatball, ham hock doner short ribs. Picanha turducken porchetta pork belly boudin hamburger. Jowl beef ribs tenderloin, shankle short ribs ribeye hamburger porchetta andouille chicken ground round cow ball tip. Salami ground round cow, rump hamburger chuck pork loin landjaeger short loin doner alcatra ball tip.'
-                }];
-            },
             link: function(/*scope, element, attrs*/) {}
         };
     });
