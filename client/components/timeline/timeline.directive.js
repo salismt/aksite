fd668c733f3e4950508bf5f85ec31e9bcd3758be
Andrew Koroluk <koroluka@gmail.com>
2015-08-28 23:36:26 -0400

    feat(timeline): add open-close functionality

--- client/components/timeline/timeline.directive.js
+++ client/components/timeline/timeline.directive.js
@@ -8,6 +8,22 @@ angular.module('aksiteApp')
             },
             templateUrl: 'components/timeline/timeline.html',
             restrict: 'EA',
-            link: function(/*scope, element, attrs*/) {}
+            link: function(scope, element/*, attrs*/) {
+                let openedCard = null;
+
+                scope.toggleCardOpen = function(card) {
+                    if(openedCard === card) {  // Clicked card is the open one, close it
+                        openedCard = null;
+                        card.open = false;
+                    } else if(openedCard) {     // Another card is open, close it and open this one
+                        openedCard.open = false;
+                        card.open = true;
+                        openedCard = card;
+                    } else {                    // No open cards, open this one
+                        card.open = true;
+                        openedCard = card;
+                    }
+                };
+            }
         };
     });
