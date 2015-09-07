56232c95b2e9149a6e4077cbe90d834826c98087
Andrew Koroluk <koroluka@gmail.com>
2015-09-07 08:58:14 -0400

    refactor(app): use $cookies

--- client/app/app.js
+++ client/app/app.js
@@ -22,23 +22,24 @@ angular.module('aksiteApp', [
         $httpProvider.interceptors.push('authInterceptor');
     })
 
-    .factory('authInterceptor', function($rootScope, $q, $cookieStore, $location) {
+    .factory('authInterceptor', function($rootScope, $q, $cookies, $injector) {
+        var state;
         return {
             // Add authorization token to headers
             request: function(config) {
                 config.headers = config.headers || {};
-                if($cookieStore.get('token')) {
-                    config.headers.Authorization = 'Bearer ' + $cookieStore.get('token');
+                if($cookies.get('token')) {
+                    config.headers.Authorization = 'Bearer ' + $cookies.get('token');
                 }
                 return config;
             },
 
             // Intercept 401s and redirect you to login
             responseError: function(response) {
-                if(response.status === 401) {
-                    $location.path('/login');
+                if (response.status === 401) {
+                    (state || (state = $injector.get('$state'))).go('login');
                     // remove any stale tokens
-                    $cookieStore.remove('token');
+                    $cookies.remove('token');
                     return $q.reject(response);
                 }
                 else {
