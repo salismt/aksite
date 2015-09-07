221c06bbaad7ad0bf85f5ccee2fd896e19941b15
Andrew Koroluk <koroluka@gmail.com>
2015-09-06 23:59:05 -0400

    feat(api:files): initial use of multer for create function

--- server/api/file/file.controller.js
+++ server/api/file/file.controller.js
@@ -83,104 +83,117 @@ exports.count = function(req, res) {
 
 // Creates a new file in the DB.
 exports.create = function(req, res) {
-    var form = gridform({db: conn.db, mongo: mongoose.mongo});
+    if(_.isEmpty(req.files)) {
+        return res.status(400).send('No files given. Send a form-data request with files on the "files" key.');
+    }
 
-    //console.log(form);
+    var filename = req.files[0].originalname;
+    console.log(req.files);
 
-    // optionally store per-file metadata
-    form.on('fileBegin', function(name, file) {
-        file.metadata = {};
+    util.saveFileFromBuffer(req.files[0].buffer, {filename})
+        .then(file => {
+            console.log(file);
+            return res.send(file);
+        });
 
-        console.log(name);
-        console.log(file);
-    });
-
-    form.parse(req, function(err, fields, files) {
-        //if(err) return handleError(res, err);
-
-        /**
-         * file.name            - the uploaded file name
-         * file.type            - file type per [mime](https://github.com/bentomas/node-mime)
-         * file.size            - uploaded file size (file length in GridFS) named "size" for compatibility
-         * file.path            - same as file.name. included for compatibility
-         * file.lastModified    - included for compatibility
-         * file.root            - the root of the files collection used in MongoDB ('fs' here means the full collection in mongo is named 'fs.files')
-         * file.id              - the ObjectId for this file
-         * @type {file}
-         */
-        var file = files.file;
-
-        if(_.isNull(file))
-            return res.status(400).send(new Error('No file'));
-
-        console.log(file);
-
-        console.log(fields);
-
-        if(!_.isEmpty(fields)) {
-            //TODO: Break this out into separate functions for code readability
-            if(fields.name && typeof fields.name == 'string') {
-                file.name = fields.name;
-            }
-            if(fields.purpose && typeof fields.purpose == 'string') {
-                file.purpose = fields.purpose;
-                if(fields.purpose.toLowerCase() === 'photo') {
-                    // Model properties
-                    var photoModel = {
-                        name: file.name,
-                        fileId: file.id
-                    };
-                    if(fields.info && typeof fields.info == 'string')
-                        photoModel.info = fields.info;
-
-                    // Thumbnail generation
-                    var stream = gfs.createReadStream({_id: file._id});
-                    stream.on('error', handleGridStreamErr(res));
-                    var img = gm(stream, file.id);
-                    img.size(function(err, size) {
-                        if(size.width > size.height) {
-                            img.shave((size.width - size.height) / 2, 0);
-                        } else {
-                            img.shave(0, (size.height - size.width) / 2);
-                        }
-                        img.scale(200, 200).quality(90);
-                        img.stream(function(err, outStream) {
-                            if(err) return res.status(500).end();
-                            else {
-                                var writestream = gfs.createWriteStream({});
-                                outStream.pipe(writestream).on('close', function(file) {
-                                    photoModel.thumbnailId = file.id;
-                                });
-                            }
-                        });
-                    });
-
-                    // Create the Photo DB instance
-                    Photo.create(photoModel, function(err, photo) {
-                        if(err) return handleError(res, err);
-                        else return res.status(201).json(photo);
-                    });
-                } else if(fields.purpose.toLowerCase() === 'image') {
-                    res.status(400).send('Error: Server not setup to handle that request.');
-                } else if(fields.purpose.toLowerCase() === 'data') {
-                    res.status(400).send('Error: Server not setup to handle that request.');
-                } else {
-                    // unorganized file
-                    var fileModel = {
-                        fileId: file.id
-                    };
-                    File.create(fileModel, function(err, photo) {
-                        if(err) return handleError(res, err);
-                        else return res.status(201).json(photo);
-                    });
-                }
-            }
-        } else {
-            return res.status(201).end();
-        }
-
-        //console.log(file.id);
-    });
+    //var form = gridform({db: conn.db, mongo: mongoose.mongo});
+    //
+    ////console.log(form);
+    //
+    //// optionally store per-file metadata
+    //form.on('fileBegin', function(name, file) {
+    //    file.metadata = {};
+    //
+    //    console.log(name);
+    //    console.log(file);
+    //});
+    //
+    //form.parse(req, function(err, fields, files) {
+    //    //if(err) return handleError(res, err);
+    //
+    //    /**
+    //     * file.name            - the uploaded file name
+    //     * file.type            - file type per [mime](https://github.com/bentomas/node-mime)
+    //     * file.size            - uploaded file size (file length in GridFS) named "size" for compatibility
+    //     * file.path            - same as file.name. included for compatibility
+    //     * file.lastModified    - included for compatibility
+    //     * file.root            - the root of the files collection used in MongoDB ('fs' here means the full collection in mongo is named 'fs.files')
+    //     * file.id              - the ObjectId for this file
+    //     * @type {file}
+    //     */
+    //    var file = files.file;
+    //
+    //    if(_.isNull(file))
+    //        return res.status(400).send(new Error('No file'));
+    //
+    //    console.log(file);
+    //
+    //    console.log(fields);
+    //
+    //    if(!_.isEmpty(fields)) {
+    //        //TODO: Break this out into separate functions for code readability
+    //        if(fields.name && typeof fields.name == 'string') {
+    //            file.name = fields.name;
+    //        }
+    //        if(fields.purpose && typeof fields.purpose == 'string') {
+    //            file.purpose = fields.purpose;
+    //            if(fields.purpose.toLowerCase() === 'photo') {
+    //                // Model properties
+    //                var photoModel = {
+    //                    name: file.name,
+    //                    fileId: file.id
+    //                };
+    //                if(fields.info && typeof fields.info == 'string')
+    //                    photoModel.info = fields.info;
+    //
+    //                // Thumbnail generation
+    //                var stream = gfs.createReadStream({_id: file._id});
+    //                stream.on('error', handleGridStreamErr(res));
+    //                var img = gm(stream, file.id);
+    //                img.size(function(err, size) {
+    //                    if(size.width > size.height) {
+    //                        img.shave((size.width - size.height) / 2, 0);
+    //                    } else {
+    //                        img.shave(0, (size.height - size.width) / 2);
+    //                    }
+    //                    img.scale(200, 200).quality(90);
+    //                    img.stream(function(err, outStream) {
+    //                        if(err) return res.status(500).end();
+    //                        else {
+    //                            var writestream = gfs.createWriteStream({});
+    //                            outStream.pipe(writestream).on('close', function(file) {
+    //                                photoModel.thumbnailId = file.id;
+    //                            });
+    //                        }
+    //                    });
+    //                });
+    //
+    //                // Create the Photo DB instance
+    //                Photo.create(photoModel, function(err, photo) {
+    //                    if(err) return handleError(res, err);
+    //                    else return res.status(201).json(photo);
+    //                });
+    //            } else if(fields.purpose.toLowerCase() === 'image') {
+    //                res.status(400).send('Error: Server not setup to handle that request.');
+    //            } else if(fields.purpose.toLowerCase() === 'data') {
+    //                res.status(400).send('Error: Server not setup to handle that request.');
+    //            } else {
+    //                // unorganized file
+    //                var fileModel = {
+    //                    fileId: file.id
+    //                };
+    //                File.create(fileModel, function(err, photo) {
+    //                    if(err) return handleError(res, err);
+    //                    else return res.status(201).json(photo);
+    //                });
+    //            }
+    //        }
+    //    } else {
+    //        return res.status(201).end();
+    //    }
+    //
+    //    //console.log(file.id);
+    //});
 };
 
 // Updates an existing file in the DB.
--- server/api/file/index.js
+++ server/api/file/index.js
@@ -3,13 +3,15 @@
 import controller from './file.controller';
 import * as auth from '../../auth/auth.service';
 import {Router} from 'express';
+import multer from 'multer';
 
 let router = new Router();
+let upload = multer({storage: multer.memoryStorage()});
 
 router.get('/', auth.appendUser(), controller.index);
 router.get('/count', auth.hasRole('admin'), controller.count);
 router.get('/:id', auth.appendUser(), controller.show);
-router.post('/', auth.hasRole('admin'), controller.create);
+router.post('/', auth.hasRole('admin'), upload.array('files'), controller.create);
 router.put('/:id', auth.hasRole('admin'), controller.update);
 router.patch('/:id', auth.hasRole('admin'), controller.update);
 router.delete('/:id', auth.hasRole('admin'), controller.destroy);
