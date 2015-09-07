7d4c63dc8d2bd51c0dcc8e5950046c15328dfeb8
Andrew Koroluk <koroluka@gmail.com>
2015-09-07 02:34:00 -0400

    chore(app): remove unused Upload service

--- client/components/Upload/Upload.service.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('aksiteApp')
-    .factory('Upload', function Auth($resource) {
-        return $resource('/api/upload/:id/:controller', {
-            id: '@_id'
-        });
-    });
