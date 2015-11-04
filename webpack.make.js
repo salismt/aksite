3a61cef3cb4eb15c6d87899777e017d3062d9458
Andrew Koroluk <koroluka@gmail.com>
2015-11-04 14:37:27 -0500

    feat(webpack): use query object syntax for babel loader, add es7.classProperties

--- webpack.make.js
+++ webpack.make.js
@@ -96,7 +96,14 @@ module.exports = function makeWebpackConfig(options) {
       // Transpile .js files using babel-loader
       // Compiles ES6 and ES7 into ES5 code
       test: /\.js$/,
-      loader: 'babel?optional[]=runtime',
+      loader: 'babel',
+      query: {
+          //presets: ['es2015'],
+          optional: [
+              'runtime',
+              'es7.classProperties'
+          ]
+      },
       exclude: /node_modules/
     }, {
       // ASSET LOADER
