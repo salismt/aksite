f52e730301078189dd4c04a53a81a15c516646a0
Andrew Koroluk <koroluka@gmail.com>
2016-01-14 12:17:51 -0500

    refactor(test): remove bower_components filter in spec.js

--- spec.js
+++ spec.js
@@ -8,11 +8,4 @@ require('angular-mocks');
 require('./client/app/app');
 
 testsContext = require.context('./client', true, /\.spec\.js$/);
-
-// I SUCK AT REGEX SO I'M DOING THIS SHIT MANUALLY
-var keys = testsContext.keys();
-keys = _.filter(keys, function(key) {
-    return key.indexOf('bower_components') === -1;
-});
-
-keys.forEach(testsContext);
+testsContext.keys().forEach(testsContext);
