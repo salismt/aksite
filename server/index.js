e3eade912da93a9a2d28f2d05649aefdab4df361
Andrew Koroluk <koroluka@gmail.com>
2016-02-09 00:11:57 -0500

    feat(server): include Reflect polyfill

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 
 // Register the Babel require hook
 require('babel-core/register');
+global.Reflect = require('harmony-reflect');    // Also affects Proxy
 
 // Export the application
 module.exports = require('./app');
