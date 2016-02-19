e66234788b088ccb09f46b82cb9a8bcb8860730b
Andrew Koroluk <koroluka@gmail.com>
2016-02-19 16:24:53 -0500

    feat(deploy): add AWS CodeDeploy config

---
+++ scripts/installdeps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt-get install git
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+source ~/.bash_profile
+source ~/.bashrc
+nvm install 4.3.0
+
+npm install -g forever gulp
