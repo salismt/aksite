ee3ffa91946386a9b32c1f014e49ca84491e1418
Andrew Koroluk <koroluka@gmail.com>
2016-02-22 21:05:42 -0500

    fix(deploy): make sure NODE_ENV is production

--- scripts/startserver.sh
+++ scripts/startserver.sh
@@ -10,4 +10,4 @@ nvm use 4.3.1
 aws s3 cp s3://aksite-deploy/aksite_env.sh /root/
 /root/aksite_env.sh
 
-forever start --minUptime 1000ms --spinSleepTime 1000ms /home/ubuntu/aksite/dist/server
+NODE_ENV=production forever start --minUptime 1000ms --spinSleepTime 1000ms /home/ubuntu/aksite/dist/server
