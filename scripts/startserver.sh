20b69f800ba0cd5728c9c8254bc792fcf4c84fe1
Andrew Koroluk <koroluka@gmail.com>
2016-02-24 12:56:18 -0500

    fix(deploy): fix startserver.sh server directory

--- scripts/startserver.sh
+++ scripts/startserver.sh
@@ -9,4 +9,4 @@ nvm use 4.3.1
 
 aws s3 cp s3://aksite-deploy/local.env.js /home/ubuntu/aksite/dist/server/config
 
-NODE_ENV=production forever start --minUptime 1000ms --spinSleepTime 1000ms /home/ubuntu/aksite/dist/server
+NODE_ENV=production forever start --minUptime 1000ms --spinSleepTime 1000ms /home/ubuntu/aksite/server
