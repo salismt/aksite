42389591703c94d8d5e8a159f04708966cc93224
Andrew Koroluk <koroluka@gmail.com>
2016-04-26 12:37:59 -0400

    refactor(mocha): swap out chai for should

--- mocha.conf.js
+++ mocha.conf.js
@@ -5,16 +5,18 @@
 require('babel-register');
 
 var chai = require('chai');
+var should = require('should');
 
 // Load Chai assertions
-global.expect = chai.expect;
+// global.expect = chai.expect;
 global.assert = chai.assert;
-chai.Should();
+global.should = should;
+// chai.Should();
 
 // Load Sinon
 global.sinon = require('sinon');
 
 // Initialize Chai plugins
-chai.use(require('sinon-chai'));
-chai.use(require('chai-as-promised'));
-chai.use(require('chai-things'));
+// chai.use(require('sinon-chai'));
+// chai.use(require('chai-as-promised'));
+// chai.use(require('chai-things'));
